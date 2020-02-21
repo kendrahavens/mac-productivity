54bf0e52334294a51173c566c2c97dd8c393ef3d
Kendra Havens <kendrahavens@Kendras-MacBook-Pro.local>
2020-02-20 18:02:15 -0800

    update readme

--- ProductivityFeatures/Program.cs
+++ ProductivityFeatures/Program.cs
@@ -9,11 +9,12 @@ namespace ProductivityFeatures
             // The tips and tricks examples are in classes in this project 
             // organized by the last few updates:
             // Visual Studio 2017 Update 15.8
-            // Visual Studio 2019 Update 16.0 Preview 1
-            // Visual Studio 2019 Update 16.0 Preview 2
-            // Visual Studio 2019 Update 16.1 Preview 1
-            // Visual Studio 2019 Update 16.1 Preview 2
-            // Use (Ctrl+ click) to click on Update158 to navigate there!
+            // Visual Studio 2019 Update 16.0
+            // Visual Studio 2019 Update 16.1
+            // Visual Studio 2019 Update 16.2
+            // Visual Studio 2019 Update 16.4
+            // Visual Studio 2019 Update 16.5
+            // Place your cursor in Update158 and type (Command + D) to navigate there!
             Update158 show = new Update158();
         }
     }
